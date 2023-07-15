5060fdefcdd6f954f3c78a10d1b9e3f3fe55c780
Dat T. Tran <datttrian@gmail.com>
2023-07-15 02:10:11 -0400

    Add middlewares and templating

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,18 @@
 import express from "express";
 
-console.log(express);
+const server = express();
+
+server.use(express.static("dist"));
+
+server.set("view engine", "ejs");
+
+server.use("/", (req, res) => {
+  res.render("index", {
+    content: "EJS is <em>cool</em>!",
+  });
+});
+
+const port = 8080;
+server.listen(port, "0.0.0.0", () => {
+  console.info(`Express server listening on port ${port}`);
+});
