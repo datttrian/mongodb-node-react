d75c41bb7a6b71c7c5788b3ae88748f43259cc04
Dat T. Tran <datttrian@gmail.com>
2023-07-16 03:59:27 -0400

    Fetch data from MongoDB

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -1,14 +1,34 @@
 import express from "express";
-import testData from "../test-data.json";
 import cors from "cors";
+import { connectClient } from "./db";
 
 const router = express.Router();
 router.use(cors());
-router.get("/contests", (req, res) => {
-  // get the data from MongoDB
-  res.send({ contests: testData });
+router.get("/contests", async (req, res) => {
+  const client = await connectClient();
+
+  const contests = await client
+    .collection("contests")
+    .find()
+    .project({
+      id: 1,
+      categoryName: 1,
+      contestName: 1,
+      _id: 0,
+    })
+    .toArray();
+
+  res.send({ contests });
 });
 
-// router.get("/contests")
+router.get("/contests/:contestId", async (req, res) => {
+  const client = await connectClient();
+  // req.params.contestId
+  console.log(req.params.contestId);
+  const contests = await client
+    .collection("contests")
+    .findOne({ id: req.params.contestId });
+  res.send({ contests });
+});
 
 export default router;
